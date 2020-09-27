d56cb81b87a863b668b6c39d0a12ded6b7a8919c
Charly <62018862+dekon1212@users.noreply.github.com>
2020-09-27 12:56:54 +0200

    Update Problem10.py

--- Problem10.py
+++ Problem10.py
@@ -1,9 +1,9 @@
 n = 2000000
-    sum, prim = 0, [True] * n
-    for p in range(2, n):
-        if prim[p]:
-            sum += p
-            print(p)
-            for i in range(p*p, n, p):
-                prim[i] = False
-    print(sum)
+summe, prim = 0, [True] * n
+for p in range(2, n):
+    if prim[p]:
+        summe += p
+        print(p)
+        for i in range(p*p, n, p):
+            prim[i] = False
+print(summe)
